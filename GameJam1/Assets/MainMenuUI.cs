306bfb3c2ba3b7642aa6d2f2ea4d16b96a1243de
Habiba-Saad <223306280+Habiba-saad@users.noreply.github.com>
2026-08-26 21:43:57 +0300

    Add main menu, tutorial and credits scenes

---
+++ GameJam1/Assets/MainMenuUI.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    // Play Button
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Credits Button
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Tutorial Button
+    public void OpenTutorial()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
