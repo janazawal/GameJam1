9ba4264f690e120dded85fa54fc20b0088d869a7
janazawal <jana.zawal@gmail.com>
2026-08-30 19:36:02 +0300

    Extra pocket

--- GameJam1/Assets/Scripts/GoalManager.cs
+++ GameJam1/Assets/Scripts/GoalManager.cs
@@ -3,43 +3,34 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 [Serializable]
 public class GoalRequirement
 {
     public Items item;
     public int requiredAmount;
 }
-
 public class GoalManager : MonoBehaviour
 {
     [SerializeField] private Pocket pocket;
     [SerializeField] private GoalRequirement[] requirements; //add as many req to win as you like 
     [SerializeField] private GoalRequirement[] extraItems;
-
     public event Action OnGoalProgressChanged;
     public event Action OnWin;
-
     private bool hasWon;
-
     public IReadOnlyList<GoalRequirement> Requirements => requirements;
     public IReadOnlyList<GoalRequirement> ExtraItems => extraItems;
-
     private void OnEnable()
     {
         pocket.OnInventoryChanged += CheckProgress;
     }
-
     private void OnDisable()
     {
         pocket.OnInventoryChanged -= CheckProgress;
     }
-
     private void Start()
     {
         CheckProgress(); // in case UI needs an initial state
     }
-
     public int GetCollectedAmount(Items item)
     {
         foreach (var slot in pocket.Slots)
@@ -49,33 +40,32 @@ public class GoalManager : MonoBehaviour
         }
         return 0;
     }
-
     public int GetExtraCollectedAmount()
     {
         return pocket != null ? pocket.ExtraSlot.quantity : 0;
     }
-
+    public Items GetExtraItem()
+    {
+        return pocket != null ? pocket.ExtraSlot.item : null;
+    }
     private void CheckProgress()
     {
-        if (hasWon) return;
-        
-
+        if (hasWon)
+        {
+            UnlockNewLevel();
+        }
         OnGoalProgressChanged?.Invoke();
-
         foreach (var req in requirements)
         {
             if (GetCollectedAmount(req.item) < req.requiredAmount)
                 return; // not all requirements met yet
         }
-
         hasWon = true;
-        UnlockNewLevel();
         OnWin?.Invoke();
     }
-
     private void UnlockNewLevel()
     {
-        if(SceneManager.GetActiveScene().buildIndex>=PlayerPrefs.GetInt("ReachedIndex"))
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
         {
             PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
             PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
